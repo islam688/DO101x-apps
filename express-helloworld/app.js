0bfa03b276c345799b32bc6fd75152a4bc548a8a
islam688 <61321950+islam688@users.noreply.github.com>
2025-10-17 19:27:39 +0300

    Change server listen address to '0.0.0.0'

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,7 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello adam!\n');
 });
 
-app.listen(8080, function () {
+app.listen(8080, '0.0.0.0', function () {
   console.log('Example app listening on port 8080!');
 });
-
