67548cadb2c1d335e6ab0a89fb280305e10fc276
islam688 <61321950+islam688@users.noreply.github.com>
2025-10-17 19:35:39 +0300

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,8 +1,24 @@
 var express = require('express');
 app = express();
 
-app.get('/', function (req, res) {
+app.get('/adam', function (req, res) {
   res.send('Hello adam!\n');
+});
+
+app.get('/anas', function(req, res) {
+ res.send('Hello anas!\n');
+});
+
+app.get('/aser', function(req, res) {
+ res.send('Hello aser!\n');
+});
+
+app.get('/alaa', function(req, res) {
+ res.send('Hello alaa!\n');
+});
+
+app.get('/', function(req, res) {
+ res.send('Hello islam!\n');
 });
 
 app.listen(8080, '0.0.0.0', function () {
